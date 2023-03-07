b0f25a4f1a05f5228971151b84c8b87e5b0e2c38
ArmandoSandino <jsandino@arkkosoft.net>
2023-03-06 20:07:32 -0600

    feat; add edit method

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -55,7 +55,7 @@ class ProductController extends Controller
      */
     public function edit(Product $product)
     {
-        //
+        return view('products.edit', compact('product') );
     }
 
     /**
